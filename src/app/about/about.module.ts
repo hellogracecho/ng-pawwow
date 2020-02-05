0fec72fc5f8f6fcf98924f7840c9b7c0c0cad0ad
Grace Cho <hellogracec@gmail.com>
2020-02-05 15:31:03 -0800

    contact, about, contents reorganized

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -6,9 +6,16 @@ import { IonicModule } from "@ionic/angular";
 
 import { AboutPageRoutingModule } from "./about-routing.module";
 import { AboutPage } from "./about.page";
+import { SharedModule } from "src/app/shared/shared.module";
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, AboutPageRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    AboutPageRoutingModule,
+    SharedModule
+  ],
   declarations: [AboutPage],
   entryComponents: []
 })
